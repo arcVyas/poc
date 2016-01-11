2d16ef78cff665ff37150a6cbe2cfbb4a2e42e61
Vyas <vyas.mohan@gmail.com>
2016-01-11 00:28:48 -0600

    apis

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var pocker = require('./pocker.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -20,8 +21,7 @@ router.get('/scheduler-app/services', function(req, res, next) {
   res.render('./search/results1/services', { title: 'Express' });
 });
 router.get('/scheduler-app/services/SR12345/agents', function(req, res, next) {
-  //res.send ("Came in")
-  res.render('./search/results1/agents', { title: 'Express' });
+  res.render('./search/results/agents', {data:pocker.getAgents(1)});
 });
 router.get('/scheduler-app/reservations/r1234', function(req, res, next) {
   //res.send ("Came in")
@@ -30,6 +30,37 @@ router.get('/scheduler-app/reservations/r1234', function(req, res, next) {
 router.get('/scheduler-app/reservations/r1234c', function(req, res, next) {
   //res.send ("Came in")
   res.render('./reservation/r1234c', { title: 'Express' });
+});
+
+router.get('/api/agents', function(req,res,next){
+  res.send(pocker.getAgents(1))
+});
+
+
+router.get('/api/reservations', function(req,res,next){
+  res.send(pocker.getReservations(1))
+});
+router.get('/api/reservations/:id', function(req,res,next){
+  var id = req.params.id
+  res.send(pocker.getReservation(id))
+});
+
+router.post('/api/reservations/create', function(req,res,next){
+  console.log("create..")
+  var id = 1
+  var agentId = req.query.agentId
+  var newId = pocker.createReservation(1,agentId)
+  res.redirect('/poc/api/reservations/'+newId);
+});
+router.post('/api/reservations/:id/confirm', function(req,res,next){
+  var id = req.params.id
+  pocker.updateReservation(id,"confirmed")
+  res.send(200)
+});
+router.post('/api/reservations/:id/complete', function(req,res,next){
+  var id = req.params.id
+  pocker.updateReservation(id,"completed")
+  res.send(200)
 });
 
 module.exports = router;
---
+++ routes/pocker.js
@@ -0,0 +1,100 @@
+var fs = require("fs");
+var moment = require('moment');
+var pocker= module.exports
+console.log("\n *START* \n");
+var agentFile=__dirname+"/../public/files/agentJson.json"
+var reservationFile=__dirname+"/../public/files/reservationJson.json"
+var agentJson = require(agentFile);
+var reservationJson = require(reservationFile);
+/*console.log(content.agents.availableNow[0].name)
+content.agents.availableNow[0].name = "Vyas Mohan"
+console.log("Output Content : \n"+ JSON.stringify(content));
+fs.writeFile('/Users/vyas/workspace/projects/vyas-node/poc/public/files/agentJson-1.json',JSON.stringify(content,null,4), function(err){
+    if(err){console.log(err);}
+});*/
+console.log("\n *EXIT* \n");
+
+pocker.writeFile = function(file, jsonObj){
+  console.log("writing file")
+  fs.writeFile(file,JSON.stringify(jsonObj,null,4), function(err){
+      if(err){console.log(err);}
+  });
+  console.log("wrote")
+}
+
+pocker.getAgents = function(serviceId){
+  console.log("pulling agent details for "+ serviceId)
+  //return JSON.stringify(agentJson)
+  return agentJson
+}
+pocker.getReservations = function(customerId){
+  console.log("pulling reservation details for "+ customerId)
+  //return JSON.stringify(agentJson)
+  return reservationJson
+}
+pocker.getReservation = function(id){
+  console.log("pulling reservation details for "+ id)
+  var reservation = reservationJson.filter(function(r) {
+    return r.id == id;
+  });
+  return reservation[0]
+}
+pocker.createReservation = function(serviceId,agentId){
+  console.log(serviceId + ":" + agentId)
+  reservationJson = require(reservationFile);
+  console.log(reservationJson)
+  var agent = agentJson.agents.availableNow.filter(function(r) {
+    return r.id == agentId;
+  });
+  console.log(agent[0])
+  if(!agent[0]){
+    agent = agentJson.agents.favorites.filter(function(r) {
+      return r.id == agentId;
+    });
+    console.log(agent[0])
+    if(!agent[0]){
+      agent = agentJson.agents.moreAgents.filter(function(r) {
+        return r.id == agentId;
+      });
+    }
+  }
+  console.log("found agent" + agent[0].name)
+
+  var appointment={}
+  var now = moment()
+  var apptDate
+  if(agent[0].wait==0){
+    apptDate = now.add(5,'m')
+  }else{
+    apptDate = now.add(agent[0].wait,'h')
+  }
+
+  appointment["date"]=apptDate.format("MMMM Do YYYY, h a")
+  appointment["status"]="draft"
+  var reservation={}
+  reservation["agent"]=agent[0]
+  reservation["appointment"]=appointment
+  reservation["id"] = reservationJson.length + 1
+  console.log(reservation)
+  reservationJson.push(reservation)
+  console.log("updated")
+  
+  pocker.writeFile(reservationFile,reservationJson)
+  return reservation["id"]
+}
+
+pocker.updateReservation = function(id,status){
+  console.log(id + ":" + status)
+  reservationJson = require(reservationFile);
+  console.log(reservationJson)
+  var reservation = reservationJson.filter(function(r) {
+    return r.id == id;
+  });
+  console.log(reservation[0])
+
+  console.log("found reservation" + reservation[0].appointment.status)
+  reservation[0].appointment.status = status
+  console.log("updated")
+  
+  pocker.writeFile(reservationFile,reservationJson)
+}
