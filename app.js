666f07eb4046cf40458100e0675b53a4951465b0
Vyas <vyas.mohan@gmail.com>
2016-02-03 14:51:51 -0600

    jcp demo

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var jcp = require('./routes/jcp');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/poc', routes);
 app.use('/users', users);
+app.use('/jcp-app', jcp);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/jcp.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var http = require('http');
+var router = express.Router();
+var pocker = require('./pocker.js')
+
+/* GET home page. */
+router.get('/coupons', function(req, resp, next) {
+  var options = {
+    host: "localhost",
+    port: 8080,
+    path: '/coupons',
+    method: 'GET'
+  };
+
+  http.request(options, function(res) {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+      console.log('BODY: ' + chunk);
+      resp.render('./jcp/coupons', {data:JSON.parse(chunk)});
+    });
+  }).end();
+
+});
+
+
+module.exports = router;
