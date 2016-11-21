85833b2bac07563995caf367c0f2c3a7ebfab1ee
Vyas <vyas.mohan@gmail.com>
2016-11-21 17:40:01 -0600

    Made it an app

--- routes/swa/ais-metrics-reports.js
+++ routes/swa/ais-metrics-reports.js
@@ -7,6 +7,46 @@ var moment = require('moment');
 var mongoUtil = require( '../../public/js/mongoutil.js' );
 var ssh = require( './ssh.js' );
 
+router.get('/',function(req, res, next) {
+  mongoUtil.connectToServer( function( err ) {
+    searchMongo("ais_metrics_report_meta",{}, function(result){
+      mongoUtil.closeConnection(function(){
+        //console.log("Connection Closed")
+      });
+      res.render('./swa/ais-metrics-reports', {data:result,req:req});
+    });
+  });
+});
+
+router.get('/:id',function(req, res, next) {
+  mongoUtil.connectToServer( function( err ) {
+    searchMongo("ais_metrics_report_meta",{id: req.params.id}, function(result){
+      ////console.log(result)
+      mongoUtil.closeConnection(function(){
+        //console.log("Connection Closed")
+      });
+      res.render('./swa/ais-metrics-report', {data:result});
+    });
+  });
+});
+
+router.post('/:id',function(req, res, next) {});
+router.delete('/:id',function(req, res, next) {});
+
+router.get('/:id/data',function(req, res, next) {
+  mongoUtil.connectToServer( function( err ) {
+    searchMongo("ais_metrics_report_data",{id: req.params.id}, function(result){
+      ////console.log(result)
+      var data={}
+      data["data"]=result
+      mongoUtil.closeConnection(function(){
+        //console.log("Connection Closed")
+      });
+      res.send(data);
+    });
+  });
+});
+
 function searchMongo(collection, query, callback){
     var db = mongoUtil.getDb();
     //console.log(collection)
@@ -18,47 +58,11 @@ function searchMongo(collection, query, callback){
       } else if (result.length) {
         //console.log('Found:', result);
       } else {
-        //console.log('No document(s) found with defined "find" criteria!');
+        console.log('No document(s) found with defined "find" criteria!');
       }
       //Close connection
       db.close();
       callback(result)
     });
 }
-router.get('/',function(req, res, next) {
-  mongoUtil.connectToServer( function( err ) {
-    searchMongo("ais_metrics_report_meta",{}, function(result){
-      mongoUtil.closeConnection(function(){//console.log("Connection Closed")});
-      res.render('./swa/ais-metrics-reports', {data:result,req:req});
-    });
-  });
-});
-router.get('/:id',function(req, res, next) {
-  mongoUtil.connectToServer( function( err ) {
-    searchMongo("ais_metrics_report_meta",{id: req.params.id}, function(result){
-      ////console.log(result)
-      mongoUtil.closeConnection(function(){//console.log("Connection Closed")});
-      res.render('./swa/ais-metrics-report', {data:result});
-    });
-  });
-});
-router.post('/:id',function(req, res, next) {
-
-});
-router.delete('/:id',function(req, res, next) {
-
-});
-router.get('/:id/data',function(req, res, next) {
-  mongoUtil.connectToServer( function( err ) {
-    searchMongo("ais_metrics_report_data",{id: req.params.id}, function(result){
-      ////console.log(result)
-      var data={}
-      data["data"]=result
-      mongoUtil.closeConnection(function(){//console.log("Connection Closed")});
-      res.send(data);
-    });
-  });
-});
-
-
 module.exports = router;
--- routes/swa/ais-metrics-uploader.js
+++ routes/swa/ais-metrics-uploader.js
@@ -14,49 +14,58 @@ router.get('/',function(req, res, next) {
 
 //POST needs body-parser (Install it from npm install body-parser)
 router.post('/generate-and-upload',function(req, res) {
-
-  var container = req.body.container
-  var date = req.body.date
-  var stTime= req.body.stTime
-  var endTime=req.body.endTime
-  var serviceName = req.body.serviceName
-  var sStrings = req.body.sStrings.split(',')
-  var metrics = req.body.metrics.split(',')
-  var env = req.body.env
-  var servers = '*ais3*'
-  if(env=="MIG"){
-    servers = '*ais3*'
-  }
-  var version = req.body.version
-  var yearOfDate = date.split('-')[0]
-  var monthOfDate = date.split('-')[1]
-  var dayOfDate = date.split('-')[2]
-  var fileName = sString+"_"+metric+"_"+date+"_"+stTime+"_"+endTime+".csv"
-
-  var searchStrings=[]
-
-  for(var i=0; i< sStrings.length; i++){
-    var sString = sStrings[i]
-    for(var j=0; j< metrics.length; j++){
-      var metric = metrics[j]
-      searchStrings.push(pass.reportCommand+' -d ' + date +' -s '+stTime+' -e '+endTime+' -g "'+sString+'" -t "'+metric+'" -r '+servers+' -p '+pass.logPath+serviceName+'-'+version+'/'+yearOfDate+'/'+monthOfDate+'/'+dayOfDate+'/ -a > ./reports/'+container+'#'+env+'#'+serviceName+'#'+sString+'#'+metric+'#'+date+'#'+stTime.replace(':','-')+'#'+endTime.replace(':','-')+'.csv &');
-    }
-  }
-  ssh.execSsh(container+pass.containerPart,searchStrings,function(fileList){
+  var switchOff=true;
+  if(switchOff){
     var urlList=[]
-    for(var i=0;i<fileList.length;i++){
-      var fileName = fileList[i]
-      fileName=fileName.replace('.csv','')
-      //console.log(fileName)
-      var url = '/swa/ais/metrics/reports/'+fileName
-      //console.log(url)
-      var anchor={}
-      anchor["text"]=fileName
-      anchor["url"]=url
-      urlList.push(anchor);
+    var anchor={}
+    anchor["text"]="Sorry.. this feature is temporarily switched off until the review with stakeholders"
+    anchor["url"]=""
+    urlList.push(anchor);
+    res.send(urlList);
+  }else{
+    var container = req.body.container
+    var date = req.body.date
+    var stTime= req.body.stTime
+    var endTime=req.body.endTime
+    var serviceName = req.body.serviceName
+    var sStrings = req.body.sStrings.split(',')
+    var metrics = req.body.metrics.split(',')
+    var env = req.body.env
+    var servers = '*ais3*'
+    if(env=="MIG"){
+      servers = '*ais3*'
     }
-    res.send(urlList)
-  })
+    var version = req.body.version
+    var yearOfDate = date.split('-')[0]
+    var monthOfDate = date.split('-')[1]
+    var dayOfDate = date.split('-')[2]
+    var fileName = sString+"_"+metric+"_"+date+"_"+stTime+"_"+endTime+".csv"
+
+    var searchStrings=[]
+
+    for(var i=0; i< sStrings.length; i++){
+      var sString = sStrings[i]
+      for(var j=0; j< metrics.length; j++){
+        var metric = metrics[j]
+        searchStrings.push(pass.reportCommand+' -d ' + date +' -s '+stTime+' -e '+endTime+' -g "'+sString+'" -t "'+metric+'" -r '+servers+' -p '+pass.logPath+serviceName+'-'+version+'/'+yearOfDate+'/'+monthOfDate+'/'+dayOfDate+'/ -a > ./reports/'+container+'#'+env+'#'+serviceName+'#'+sString+'#'+metric+'#'+date+'#'+stTime.replace(':','-')+'#'+endTime.replace(':','-')+'.csv &');
+      }
+    }
+    ssh.execSsh(container+pass.containerPart,searchStrings,function(fileList){
+      var urlList=[]
+      for(var i=0;i<fileList.length;i++){
+        var fileName = fileList[i]
+        fileName=fileName.replace('.csv','')
+        //console.log(fileName)
+        var url = '/swa/ais/metrics/reports/'+fileName
+        //console.log(url)
+        var anchor={}
+        anchor["text"]=fileName
+        anchor["url"]=url
+        urlList.push(anchor);
+      }
+      res.send(urlList)
+    })
+  }
 });
 
 module.exports = router;
